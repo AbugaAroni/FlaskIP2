5445e1f5ce67525e00b6019b66edebf35130bf4a
AbugaAroni <abugaaroni@gmail.com>
2020-09-14 22:53:36 +0300

    change how retrieve function works

--- app/config.py
+++ app/config.py
@@ -2,7 +2,10 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_BASE_URL ='http://newsapi.org/v2/{}?country=us&apikey={}'
+    NEWS_API_BASE_URL ='http://newsapi.org/v2/top-headlines?{}&apikey={}'
+
+#    http://newsapi.org/v2/top-headlines?country=us&apiKey=d9eecd93059443ebb15b5d47f09717fd
+#    http://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=d9eecd93059443ebb15b5d47f09717fd
 
 class ProdConfig(Config):
     '''
--- app/views.py
+++ app/views.py
@@ -10,9 +10,9 @@ def index():
     View root page function that reutns the home new page (index) and its data
     '''
     # Getting top headline news
-    top_headlines = get_news('top-headlines')
+    top_headlines = get_news('country=us')
     print(top_headlines)
-    title = 'Home - View the Top News Stories'
+    title = 'View the Top News Stories'
     return render_template('index.html', title = title,headlines = top_headlines)
 
 @app.route('/news_source/<int:news_id>')
