dd5793c2dcfe59affec5e7367d1e6210954456c6
AbugaAroni <abugaaroni@gmail.com>
2020-09-14 17:56:39 +0300

    add test to see if object is initialized correctly

--- app/news_test.py
+++ app/news_test.py
@@ -20,5 +20,18 @@ class NewsTest(unittest.TestCase):
         self.assertTrue(isinstance(self.new_News,News))
 
 
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_News.id,1234)
+        self.assertEqual(self.new_News.title,"BBC")
+        self.assertEqual(self.new_News.description,"Brexit happened because zyz")
+        self.assertEqual(self.new_News.image,"https://ichef.bbci.co.uk/images/ic/1024x576/p08rdfbt.jpg")
+        self.assertEqual(self.new_News.source,"https://www.cnn.com/2020/09/14/politics/election-2020-ballot-signature-mismatches/index.html")
+        self.assertEqual(self.new_News.date,"2020-09-14T12:00:00Z")
+
+
 if __name__ == '__main__':
     unittest.main()
